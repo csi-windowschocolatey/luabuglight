df8e892278cacd3afef23e81ccd4d7ee427cea21
DarwinJS <darwinjsgitlab.services@sanoys.com>
2015-11-28 07:22:58 -0500

    Updated to luabuglight 2.3

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,9 +1,16 @@
-﻿$packageName = 'LuaBuglight' # arbitrary name for the package, used in messages
+$PackageName = 'LuaBuglight'
 $url = 'http://blogs.msdn.com/cfs-file.ashx/__key/communityserver-components-postattachments/00-10-62-49-95/LuaBuglight.zip' # download url
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+$AppPathKey = "Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\$exeName"
+If (!(Test-Path $AppPathKey)) {New-Item "$AppPathKey" | Out-Null}
+Set-ItemProperty -Path $AppPathKey -Name "(Default)" -Value "$env:chocolateyinstall\lib\$packagename\tools\$exeName"
+Set-ItemProperty -Path $AppPathKey -Name "Path" -Value "$env:chocolateyinstall\lib\$packagename\tools\"
+If (Test-Path "$env:chocolateyinstall\bin\$exeName") {Remove-Item "$env:chocolateyinstall\bin\$exeName" -Force -EA SilentlyContinue}
+
 Write-Output "***********************************************************************************************************************"
-Write-Output "*  INSTRUCTIONS: In a new, non-admin prompt or in explorer, type `"luabuglight.exe`" to monitor to start luabuglight. *"
+Write-Output "*  INSTRUCTIONS: In a new, non-admin prompt or in explorer, type `"luabuglight.exe`" to start luabuglight.            *"
+Write-Output "*  You may also type `"$exeName`" In the search prompt of your start menu                                             *"
 Write-Output "***********************************************************************************************************************"
