2ef0cdbaf4f575cb9842a1ceac8e5747c873b5d0
DarwinJS <darwinjsgitlab.services@sanoys.com>
2015-11-28 06:41:23 -0500

    Updated for 2.3

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'LuaBuglight' # arbitrary name for the package, used in messages
-$url = 'http://blogs.msdn.com/cfs-file.ashx/__key/communityserver-components-postattachments/00-10-37-28-54/LuaBuglight.zip' # download url
+$url = 'http://blogs.msdn.com/cfs-file.ashx/__key/communityserver-components-postattachments/00-10-62-49-95/LuaBuglight.zip' # download url
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
